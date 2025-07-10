3a31fb755d55a7e22c9293defd2a56fea183d801
reversed-R <qiu1287shan@gmail.com>
2025-07-10 15:51:19 +0900

    fix: rust auto format on save

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -46,21 +46,23 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
-      
-      rust_analyzer = function(_, _) require("lspconfig").rust_analyzer.setup({
+      rust_analyzer = {
+        capabilities = {
+          documentFormattingProvider = true,
+        },
         settings = {
           ["rust-analyzer"] = {
             check = {
               command = "clippy",
             },
           },
-        },
-      }) end
+        }
+      }
+    },
+    -- customize how language servers are attached
+    handlers = {
+      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
+      -- function(server, opts) require("lspconfig")[server].setup(opts) end
 
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
